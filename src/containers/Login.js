24e8d65013548bd145a213d24b8caaaadb539335
Marin <marin.knezevic1et2@gmail.com>
2022-03-31 20:00:01 +0200

    just commenting

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -4,6 +4,10 @@ import Button from "react-bootstrap/Button";
 import "./Login.css";
 
 //using usestate hooks to store what users enter in form
+//then we connect state to our two fields in the form(setemail,setpass) functions to store what users type in
+//then we are setting form controls to show the value of our two variables(email,pass)
+//validateform checks if our forms are empty
+
 export default function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");

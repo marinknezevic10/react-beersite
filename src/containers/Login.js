b2e6c8f0550d5ad62b641c8f285f78faecb4a943
Marin <marin.knezevic1et2@gmail.com>
2022-03-31 19:55:25 +0200

    comment

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -3,6 +3,7 @@ import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import "./Login.css";
 
+//using usestate hooks to store what users enter in form
 export default function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
